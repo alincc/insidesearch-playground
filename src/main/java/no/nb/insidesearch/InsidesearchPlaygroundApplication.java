4d5ad547f9ceb35ab50eccde7a2a25b056ede1a7
ronnymikalsen <ronny.mikalsen@nb.no>
2016-01-20 21:10:00 +0100

    created

---
+++ src/main/java/no/nb/insidesearch/InsidesearchPlaygroundApplication.java
@@ -0,0 +1,12 @@
+package no.nb.insidesearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsidesearchPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsidesearchPlaygroundApplication.class, args);
+	}
+}
---
+++ src/main/java/no/nb/insidesearch/StaticResourceConfiguration.java
@@ -0,0 +1,18 @@
+package no.nb.insidesearch;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/insidesearch-playground/dist/" };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+}
