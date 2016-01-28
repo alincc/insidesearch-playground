18b8ab5defb218e5a64236f223c8a7956c6e838a
ronnymikalsen <ronny.mikalsen@nb.no>
2016-01-28 23:02:21 +0100

    Search Settings

--- insidesearch-playground-webapp/src/main/resources/insidesearch-playground/src/app/components/search.component/search.component.ts
+++ insidesearch-playground-webapp/src/main/resources/insidesearch-playground/src/app/components/search.component/search.component.ts
@@ -7,17 +7,18 @@ import {FORM_DIRECTIVES} from 'angular2/common';
 
 import {SearchBoxComponent} from '../search-box.component/search-box.component';
 import {SearchResultsComponent} from '../search-results.component/search-results.component';
+import {SearchSettingsComponent} from '../search-settings.component/search-settings.component';
+
 import {SearchModel, SearchResult, Item} from '../../services/nb.service/nb.service';
 
 declare var componentHandler;
-declare var dialogPolyfill;
 
 @Component({
   selector: 'search.component',
   templateUrl: 'app/components/search.component/search.component.html',
   styleUrls: ['app/components/search.component/search.component.css'],
   providers: [],
-  directives: [SearchBoxComponent, SearchResultsComponent, MATERIAL_DIRECTIVES, ROUTER_DIRECTIVES, FORM_DIRECTIVES],
+  directives: [SearchBoxComponent, SearchResultsComponent, SearchSettingsComponent, MATERIAL_DIRECTIVES, ROUTER_DIRECTIVES, FORM_DIRECTIVES],
   pipes: []
 })
 export class SearchComponent implements OnInit {
@@ -25,31 +26,6 @@ export class SearchComponent implements OnInit {
     searchModel = new SearchModel('', 100, 'Alle', true, true, false, false);
     results: SearchResult;
     compare: Item[] = [];
-    
-    mediatypes: any[] = [
-        {label: 'Alle', value:'Alle'},
-        {label: 'Aviser', value:'Aviser'},
-        {label: 'Bøker', value:'Bøker'},
-        {label: 'Foto', value:'Bilder'},
-        {label: 'Film', value:'Film'},
-        {label: 'Fjernsyn', value:'Fjernsyn'},
-        {label: 'Radio', value:'Radio'},
-        {label: 'Musikk', value:'Musikk'},
-        {label: 'Programrapporter', value:'Programrapporter'},
-        {label: 'Lydopptak', value:'Lydopptak'},
-        {label: 'Musikkmanuskripter', value:'Musikkmanuskripter'},
-        {label: 'Privatarkiv', value:'Privatarkivmateriale'},
-        {label: 'Kart', value:'Kart'},
-        {label: 'Noter', value:'Noter'},
-        {label: 'Tidsskrift', value:'Tidsskrift'},
-        {label: 'Gjenstander', value:'Gjenstander'},
-        {label: 'Artikler', value:'Artikler'},
-        {label: 'Plakater', value:'Plakater'},
-        {label: 'Nettsider', value:'Nettsider'},
-        {label: 'Punktskrift', value:'Punktskrift'},
-        {label: 'Annet', value:'Annet'},
-        {label: 'Ukjent', value:'Ukjent'},
-    ];
 
     constructor() {
     }
@@ -70,21 +46,8 @@ export class SearchComponent implements OnInit {
         componentHandler.upgradeAllRegistered();
     }
     
-    showAddToFavoritesDialog(): void {
-        console.log('tester');    
-        var dialog:any = document.querySelector('dialog');
-        dialog.showModal();
-        
-    }
-    
     ngOnInit(): void {
         componentHandler.upgradeAllRegistered();
-
-        var dialog:any = document.querySelector('dialog');
-        var showDialogButton = document.querySelector('#show-dialog');
-        if (! dialog.showModal) {
-        dialogPolyfill.registerDialog(dialog);
-        }
     }
 }
 
